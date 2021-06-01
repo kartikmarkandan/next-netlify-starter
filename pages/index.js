e1d9c2d4b0c225e18216690b80e3b33214793c7e
Kartik Markandan <kartik.markandan.1@gmail.com>
2021-06-01 14:51:48 -0700

    add home content

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,55 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head"
+// import { Component } from 'react'
+import React from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!  From Kartik</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// export default class Home extends Component {
+//   render() {
+//     let { title, cats } = attributes;
+//     return (
+//       <>
+//         <Head>
+//           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+//         </Head>
+//         <article>
+//           <h1>{title}</h1>
+//           <HomeContent />
+//           <ul>
+//             {cats.map((cat, k) => (
+//               <li key={k}>
+//                 <h2>{cat.name}</h2>
+//                 <p>{cat.description}</p>
+//               </li>
+//             ))}
+//           </ul>
+//         </article>
+//       </>
+//     )
+//   }
+// }
+const Home = () => {
+  
+  let { title, cats } = attributes;
+    return (
+      <>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </>
+    )
+  }
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-          Hey There!  This is Kartik Saying Hello!
-        </p>
-      </main>
 
-      <Footer />
-    </div>
-  )
-}
+export default Home
